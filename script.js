73f09e6409cb7fa756d0ab0c77d6fd449813a6a1
CodeSevenic <sbu.audio.production@gmail.com>
2022-04-04 13:38:55 +0200

    code update

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $('.navbar-toggler').click(function() {
+    $('.navbar-collapse').slideToggle(300);
+  });
+});
+
+const subMenus = $('.sub-menu');
+const menuLinks = $('.menu-link');
+
+function smallScreenMenu() {}
